93cee0f38f0f59e4e4dbea00e2a9f3a163619a20
Zichen-Wang <zichen.wang@pku.edu.cn>
2018-06-12 20:26:50 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 gcc CUTE/demo/c_lib/libsim.c -o CUTE/demo/c_lib/libsim.so -fPIC -shared -pthread -O3
 gcc CUTE/demo/c_lib/libpath.c -o CUTE/demo/c_lib/libpath.so -fPIC -shared -O3
 
-cp -r $2 CUTE/
+if [ -n "$2" ]
+then
+    cp -r $2 CUTE/
+fi
 
 source venv/bin/activate
 python CUTE/manage.py runserver $1
